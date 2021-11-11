69bae8b71794515095d72280bdf7d1725cfc9e76
Jesús Millán <jgmc3012@gmail.com>
2021-11-11 16:01:21 -0400

    feat: Add Request rest for task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,10 +24,10 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.owner = current_user
-
+    
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: "Task was successfully created." }
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: "Task was successfully updated." }
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
+      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -71,7 +71,7 @@ class TasksController < ApplicationController
         :description,
         :due_date,
         :category_id,
-        participating_users: %i[ user_id role id _destroy ]
+        participating_users_attributes: %i[ user_id role id _destroy ]
       )
     end
 end
---
+++ spec/requests/tasks_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe "Tasks", type: :request do
+  let(:user) { create(:user) }
+  before(:each) { sign_in user } # Devise helper
+  describe "GET /tasks" do
+    it "works! (now write some real specs)" do
+      get tasks_path
+      expect(response).to have_http_status(200)
+    end
+  end
+  describe 'GET /task/new' do
+    it "works! (now write some real specs)" do
+      get new_task_path
+      expect(response).to render_template(:new)
+    end
+  end
+  describe 'POST /task' do
+    let(:category) { create(:category) }
+    let(:participant_user) { create(:user) }
+    let(:params) do
+      {
+        task: {
+          name: Faker::Lorem.sentence,
+          due_date: Date.today + 1.week,
+          category_id: category.id.to_s,
+          description: Faker::Lorem.paragraph,
+          participating_users_attributes: [
+            {
+              user_id: participant_user.id.to_s,
+              role: '1',
+              _destroy: false
+            }
+          ]
+        }
+      }
+    end
+
+    it 'create a new task and redirect to show page' do
+      post tasks_path, params: params
+
+      expect(response).to redirect_to(assigns(:task)) # assigns(:task) is a task object instance created in the life of the request. 'assigns' method is available in test type: request.
+      follow_redirect!
+      expect(response).to render_template(:show)
+      expect(response.body).to include('Task was successfully created.')
+    end
+  end
+end
